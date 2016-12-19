35544dc6c36f64a98338ff84078215413fd70ff0
AndreiCalazans <andreixoc@hotmail.com>
2016-12-19 09:33:21 -0200

    final implements

--- js.js
+++ js.js
@@ -1,15 +1,24 @@
+// cssClock.surge.sh
+
 function clock() {
   var time = new Date();
 
   //transform the seconds to deg--
-  var secondsDeg = (time.getSeconds() / 60 * 360) -90;
-  var minutesDeg = (time.getMinutes()/60 *360) - 90;
-  var hour = (time.getHours() / 12 * 360) -90;
+  var hour = time.getHours();
+  var minutes = time.getMinutes();
+  var seconds = time.getSeconds();
 
+  var secondsDeg = (seconds / 60 * 360) -90;
+  var minutesDeg = (minutes/60 *360) - 90;
+  var hourDeg = (hour / 12 * 360) -90;
+  hour = (hour < 10 )? "0" + hour : hour;
+  minutes = (minutes < 10 )? "0" + minutes : minutes;
+  seconds = (seconds < 10 )? "0" + seconds : seconds;
 
+  document.getElementById("time").innerHTML = hour +" : "+minutes+" : "+seconds;
   document.querySelector(".second-hand").style.transform = "rotate("+ secondsDeg +"deg)";
   document.querySelector(".minute-hand").style.transform = "rotate("+ minutesDeg +"deg)";
-  document.querySelector(".hour-hand").style.transform = "rotate("+ hour +"deg)";
+  document.querySelector(".hour-hand").style.transform = "rotate("+ hourDeg +"deg)";
 
 }
 setInterval(clock,1000);
